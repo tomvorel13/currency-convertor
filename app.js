5d3a6f591593bfb23342d74c8aa3df9b902f2689
Tom Vorel <tomvorel13@gmail.com>
2018-01-24 19:50:42 +0100

    Working on converting from both currencies

--- app.js
+++ app.js
@@ -5,9 +5,25 @@ const inputUSD = document.querySelector("#usd");
 const button = document.querySelector(".btn");
 const result = document.querySelector("#result");
 const popup = document.querySelector("#popup");
+const currOption = document.querySelector("#currOption");
+const optCZK = document.querySelector("#optCZK");
+const optUSD = document.querySelector("#optUSD");
 
 //Other variables
 let usdRate = 0.0483675;
+
+//Function for changing options
+function changeOptions(opt1, opt2) {
+	opt1.classList.add("selected");
+	opt2.classList.remove("selected");
+}
+
+optUSD.addEventListener("click", () => {
+	changeOptions(optUSD, optCZK);
+});
+optCZK.addEventListener("click", () => {
+	changeOptions(optCZK, optUSD);
+});
 
 button.addEventListener("click", e => {
 	if (inputCZ.value === "" || inputCZ.value === isNaN) {
@@ -16,12 +32,13 @@ button.addEventListener("click", e => {
 		popup.textContent = "Not a valid number";
 
 		setTimeout(popupGone, 2000);
-	} else {
+	} else if (optCZK.classList.contains("selected")) {
 		let finalAmount = (inputCZ.value * usdRate).toFixed(2);
-		finalAmount = `$${finalAmount}`;
-		//Display the result
-		result.innerHTML = `<h1 class="display-1 text-center">${finalAmount}</h1>`;
-		//Reset form
+		result.innerHTML = `<h1 class="display-1 text-center">$${finalAmount}</h1>`;
+		inputCZ.value = "";
+	} else if (optUSD.classList.contains("selected")) {
+		let finalAmount = (inputCZ.value / usdRate).toFixed();
+		result.innerHTML = `<h1 class="display-1 text-center">${finalAmount} CZK</h1>`;
 		inputCZ.value = "";
 	}
 
